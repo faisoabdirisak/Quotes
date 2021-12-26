4ad90c6aa902d1bf0408a4e7bd41f43691482d05
faisoabdirisak <faisoabdirisak@gmail.com>
2021-12-26 14:21:35 +0300

    it will add upvote and downvote properties

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,8 @@
 export class Quote {
+    public upvotes:number;
+    public downvotes:number;
     constructor(public name:string, public author:string, public qoute:string, public myDate:Date){
-    
+        this.upvotes=0;
+        this.downvotes=0;
     }
 }
