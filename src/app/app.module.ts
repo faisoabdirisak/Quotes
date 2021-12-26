3a2c81a97869ce10f7046b0f5bddac20a9387e63
faisoabdirisak <faisoabdirisak@gmail.com>
2021-12-26 13:41:47 +0300

    it will add quote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule,
