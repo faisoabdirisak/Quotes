496799c3da64e87c93c3c13b48cc79f4c448a9a1
faisoabdirisak <faisoabdirisak@gmail.com>
2021-12-26 15:09:34 +0300

    it will add directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { FormComponent } from './form/form.component';
 import { FormsModule } from '@angular/forms';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { FormsModule } from '@angular/forms';
     QuoteComponent,
     QuoteDetailComponent,
     FormComponent,
+    HighlightDirective,
   
   ],
   imports: [
